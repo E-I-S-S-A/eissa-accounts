26697ef495691fc01a1c668d2dd108140870dbb0
Eissa Faheem <eissafaheem@gmail.com>
2024-10-31 16:18:51 +0530

    feat: added validators in signup form

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../constants/routes";
+import { REGEXES } from "../../constants/regexes";
 
 type FormData = {
     firstName: string;
@@ -25,28 +26,39 @@ const Signup = () => {
         register,
         handleSubmit,
         formState: { errors, touchedFields },
+        trigger
     } = useForm<FormData>({ mode: "all" });
 
     const { step, setStep } = useOutletContext<SignupContext>();
 
     useEffect(() => {
         return onUnmount();
-    }, [])
+    }, []);
 
     const onUnmount = () => {
         setStep(1);
-    }
+    };
 
     const onBackPress = () => {
-        setStep(prev => prev - 1)
-    }
+        setStep(prev => prev - 1);
+    };
 
-    const onSubmit = (data: FormData) => {
-        if (step === 4) {
-            console.log(data);
+    const onSubmit = async (data: FormData) => {
+        const fieldsToValidate: { [key: number]: (keyof FormData)[] } = {
+            1: ["firstName"],
+            2: ["email"],
+            3: ["otp"],
+            4: ["password", "confirmPassword"]
+        };
+
+        const isValid = await trigger(fieldsToValidate[step]);
+
+        if (isValid) {
+            if (step === 4) {
+                console.log(data);
+            }
+            setStep(prev => prev + 1);
         }
-
-        setStep(prev => prev + 1);
     };
 
     const Steps = () => {
@@ -59,10 +71,11 @@ const Signup = () => {
                             name="firstName"
                             register={register}
                             error={errors?.firstName}
+                            rules={{ required: { message: "First name is required", value: true } }}
                             isTouched={touchedFields?.firstName}
                         />
                         <EissaInputField
-                            label="Last name"
+                            label="Last name (optional)"
                             name="lastName"
                             register={register}
                             error={errors?.lastName}
@@ -78,6 +91,12 @@ const Signup = () => {
                             name="email"
                             register={register}
                             error={errors?.email}
+                            rules={{
+                                required: { message: "Email is required", value: true }, pattern: {
+                                    message: "Invalid email",
+                                    value: REGEXES.email,
+                                },
+                            }}
                             isTouched={touchedFields?.email}
                         />
                     </>
@@ -91,6 +110,7 @@ const Signup = () => {
                             name="otp"
                             register={register}
                             error={errors?.otp}
+                            rules={{ required: { message: "OTP is required", value: true } }}
                             isTouched={touchedFields?.otp}
                         />
                     </>
@@ -104,6 +124,7 @@ const Signup = () => {
                             name="password"
                             register={register}
                             error={errors?.password}
+                            rules={{ required: { message: "Password is required", value: true } }}
                             isTouched={touchedFields?.password}
                         />
                         <EissaInputField
@@ -111,6 +132,7 @@ const Signup = () => {
                             name="confirmPassword"
                             register={register}
                             error={errors?.confirmPassword}
+                            rules={{ required: { message: "Confirm your password", value: true } }}
                             isTouched={touchedFields?.confirmPassword}
                         />
                     </>
@@ -134,7 +156,6 @@ const Signup = () => {
                     }
                 </div>
                 <div className={styles.buttons}>
-
                     {
                         step > 1 &&
                         <EissaButton
